fc6610d8069e0449c30cba745fc0c9750eaaa60b
Edward Reyes <lopezredd@gmail.com>
2020-12-03 08:47:12 +0900

    --wip-- [skip ci]

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'input-file-js'
+import 'input-file-js/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
